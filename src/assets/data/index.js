4e9a57a719c961945d2942a5ac5b6b4d9b0b8e5f
dnvthao <https://github.com/dnvthao>
2023-03-15 11:37:58 +0700

    init first

---
+++ src/assets/data/index.js
@@ -0,0 +1,24 @@
+const Data = [
+  {
+    id: 1,
+    title: 'Tuổi 20 zui',
+    artist: 'Nguyễn Hoàng Phúc',
+    thumbnail: require('../images/SN.jpg'),
+    src: require('../musics/Happy BirthDay To Phuc.mp3')
+  },
+  {
+    id: 2,
+    title: 'Chồng chị hát sinh nhật em nè',
+    artist: 'Chị thay Lợi chúc mừng sinh nhật em',
+    thumbnail: require('../images/sn3.jpg'),
+    src: require('../musics/SinhNhat-HieuMonday.mp3')
+  },
+  {
+    id: 3,
+    title: 'Quẫy lên em eeeee',
+    artist: 'Chúc niềng răng mau bung mắc cài',
+    thumbnail: require('../images/sn2.jpg'),
+    src: require('../musics/BirhDay - Somi.mp3')
+  },
+]
+  export default Data;
